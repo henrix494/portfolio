db1d995c68c4a4c8a22b90a5be315153788c712c
henrix494 <henrix494@gmail.com>
2024-01-15 18:53:30 +0200

    first commit

--- components/AboutMe/Skilss.tsx
+++ components/AboutMe/Skilss.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React from "react";
 import "./movingNoise.css";
 import { useInView } from "react-intersection-observer";
@@ -15,33 +13,32 @@ const Skills = () => {
     { title: "No-Sql", percent: 99 },
   ];
 
+  // useInView hook outside the map function
+  const { ref, inView } = useInView({
+    threshold: 1,
+    delay: 0,
+  });
+
   return (
-    <div className=" lg:mt-0 max-lg:mt-10">
+    <div className="lg:mt-0 max-lg:mt-10">
       <div className="progress-bar text-center flex h-screen max-lg:h-max">
         <h1 className="text-4xl overflow-hidden mb-5 lg:mb-0">הכלים שלי</h1>
-        {skillsData.map((skill, index) => {
-          const { ref, inView } = useInView({
-            threshold: 1,
-            delay: 0,
-          });
-
-          return (
-            <div
-              className="skills flex flex-col text-white items-center text-center  "
-              key={index}
-            >
-              <div></div>
-              <div className="skill-title">{skill.title}</div>
-              <div className="bar">
-                <div
-                  ref={ref}
-                  className={`bar-color ${inView ? "animate-width" : ""}`}
-                  style={{ width: inView ? `${skill.percent}%` : "0%" }}
-                ></div>
-              </div>
+        {skillsData.map((skill, index) => (
+          <div
+            className="skills flex flex-col text-white items-center text-center"
+            key={index}
+          >
+            <div></div>
+            <div className="skill-title">{skill.title}</div>
+            <div className="bar">
+              <div
+                ref={ref} // Use the ref here
+                className={`bar-color ${inView ? "animate-width" : ""}`}
+                style={{ width: inView ? `${skill.percent}%` : "0%" }}
+              ></div>
             </div>
-          );
-        })}
+          </div>
+        ))}
       </div>
     </div>
   );
