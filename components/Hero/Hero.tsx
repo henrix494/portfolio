930858204ba6ca0bedc5888b1bd662d7357c92e0
henrix494 <henrix494@gmail.com>
2024-01-15 19:40:54 +0200

    first commit

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -11,14 +11,14 @@ export default function Hero() {
       <div className=" overflow-hidden">
         <div className="z-[20]   relative text-white text-[12rem] max-lg:text-[4rem] flex flex-col justify-center items-center h-screen ">
           <motion.div
-            className="absolute top-40 z-40 bg-[white] h-[2px] w-0  sm:w-0 lg:max-w-[55%] max-lg:top-44"
+            className="absolute top-40 z-40 bg-[white] h-[2px] w-0  sm:w-0 lg:max-w-[55%] max-lg:top-52"
             animate={{ width: "80vw" }} // final state
             transition={{ duration: 3, ease: "easeInOut" }} // animation settings
           ></motion.div>
           <h2>מפתח תוכנה</h2>
           <h1 className="text-center">Full Stack</h1>
           <motion.div
-            className="absolute bottom-40 z-40 bg-[white] h-[2px] w-0  sm:w-0 lg:max-w-[55%] max-lg:bottom-44"
+            className="absolute bottom-40 z-40 bg-[white] h-[2px] w-0  sm:w-0 lg:max-w-[55%] max-lg:bottom-52"
             animate={{ width: "80vw" }} // final state
             transition={{ duration: 3, ease: "easeInOut" }} // animation settings
           ></motion.div>
