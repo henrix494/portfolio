9640d35cff84e36a9fed29d018abb9b2a7248cb8
henrix494 <henrix494@gmail.com>
2024-01-15 19:37:35 +0200

    first commit

--- components/AboutMe/Skilss.tsx
+++ components/AboutMe/Skilss.tsx
@@ -9,6 +9,24 @@ const useSkillInView = () => {
   });
 };
 
+const SkillComponent = ({ skill }: any) => {
+  const { ref, inView } = useSkillInView();
+
+  return (
+    <div className="skills flex flex-col text-white items-center text-center">
+      <div></div>
+      <div className="skill-title">{skill.title}</div>
+      <div className="bar">
+        <div
+          ref={ref}
+          className={`bar-color ${inView ? "animate-width" : ""}`}
+          style={{ width: inView ? `${skill.percent}%` : "0%" }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
 const Skills = () => {
   const skillsData = [
     { title: "Html", percent: 99 },
@@ -24,35 +42,12 @@ const Skills = () => {
     <div className="lg:mt-0 max-lg:mt-10">
       <div className="progress-bar text-center flex h-screen max-lg:h-max">
         <h1 className="text-4xl overflow-hidden mb-5 lg:mb-0">הכלים שלי</h1>
-        {skillsData.map((skill, index) => {
-          const { ref, inView } = useSkillInView();
-
-          return (
-            <SkillComponent
-              key={index}
-              skill={skill}
-              ref={ref}
-              inView={inView}
-            />
-          );
-        })}
+        {skillsData.map((skill, index) => (
+          <SkillComponent key={index} skill={skill} />
+        ))}
       </div>
     </div>
   );
 };
-
-const SkillComponent = ({ skill, ref, inView }: any) => (
-  <div className="skills flex flex-col text-white items-center text-center">
-    <div></div>
-    <div className="skill-title">{skill.title}</div>
-    <div className="bar">
-      <div
-        ref={ref}
-        className={`bar-color ${inView ? "animate-width" : ""}`}
-        style={{ width: inView ? `${skill.percent}%` : "0%" }}
-      ></div>
-    </div>
-  </div>
-);
 
 export default Skills;
