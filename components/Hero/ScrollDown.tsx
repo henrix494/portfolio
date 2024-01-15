361bb67b81b642362aef6978f0078adec3f6141e
henrix494 <henrix494@gmail.com>
2024-01-15 19:43:22 +0200

    first commit

--- components/Hero/ScrollDown.tsx
+++ components/Hero/ScrollDown.tsx
@@ -1,7 +1,7 @@
 import "./scrollDown.css";
 export default function ScrollDown() {
   return (
-    <div className=" absolute bottom-5 right-1/2 z-30 translate-x-[50%]">
+    <div className=" absolute bottom-0 right-1/2 z-30 translate-x-[50%]">
       <div className="mouse"></div>
     </div>
   );
