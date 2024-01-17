dbc57079a184923d3f065f61920bbef26adb115b
henrix494 <henrix494@gmail.com>
2024-01-17 20:27:42 +0200

    added analitics

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="heb" dir="rtl">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
